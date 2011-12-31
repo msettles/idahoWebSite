7c42a23033326c1e07486b97775047f1ee1ba0b3
msettles <msettles@uidaho.edu>
2011-12-30 19:39:33 -0800

    Version 1.0

--- js/script.js
+++ js/script.js
@@ -1,18 +1,14 @@
-/* Author: 
+/* Author: Matt Settles
 
 */
-
-
-
-
-
-
-
-
-
-
-
-
+$(document).ready(function(){
+  // This code is executed after the DOM has been completely loaded
+  $('nav a,footer a.up').click(function(e){
+  // If a link has been clicked, scroll the page to the link's hash target:
+    $.scrollTo( this.hash || 0, 1500);
+    e.preventDefault();
+  });
+});
 
 
 
